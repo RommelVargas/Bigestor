b50b72ce4ec770ede0fe8338360e55a0b37da7e2
RommelVargas <rommelvarmey@gmail.com>
2025-09-25 23:02:09 -0600

    render1

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviroment.get('secret_key', default='django-in')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-jdh-r!ihbj#u6xea(o)h8y(bxjl957w0#rz3pegil_yq5nee!&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
